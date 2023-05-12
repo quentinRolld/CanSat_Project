29581fed551e799a24bade365390306616968feb
quentinRolld <quantinrld@gmail.com>
2023-05-12 14:45:29 +0200

    servos

---
+++ Cansat_final_1/Core/Inc/servo.h
@@ -0,0 +1,23 @@
+/*
+ * servo.h
+ *
+ *  Created on: 11 mai 2023
+ *      Author: quentinrolland
+ */
+
+#ifndef INC_SERVO_H_
+#define INC_SERVO_H_
+
+
+#include "main.h"
+#define minAngle 70
+#define maxAngle 431
+void servoSetAngleLeft(unsigned int angle);
+void servoSetAngleRight(unsigned int angle);
+void servoSetPositionLeft (int position);
+void servoSetPositionRight (int position);
+void choice_direction_intensity(unsigned int delta_teta);
+
+
+
+#endif /* INC_SERVO_H_ */
